06532a46a95922fa06c1483cb6601ee22ef25074
Aakash Kushwaha <aakaashkumar898@gmail.com>
2026-03-03 22:06:05 +0530

    Time: 162 ms (10.21%) | Memory: 59.9 MB (11.78%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,42 +1,46 @@
 class Solution {
-    private String reverse(String s){
-        int n=s.length();
-        StringBuilder sb=new StringBuilder();
-        for(int i=n-1; i>=0; i--){
-            char ch=s.charAt(i);
-            sb.append(ch);
-        }
-        String str=invert(sb.toString());
-        return str;
-    }
-    private String invert(String s){
-        int n=s.length();
-        StringBuilder sb=new StringBuilder();
-        for(int i=0; i<n ; i++){
-            char ch=s.charAt(i);
-            if(ch=='0'){
-                sb.append(1);
-            
-            }else{
-                sb.append(0);
-            }
 
+    private String reverse(StringBuilder s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = n - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
         }
+
+        return invert(sb.toString());
+    }
+
+    private String invert(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '0') {
+                sb.append('1');   
+            } else {
+                sb.append('0');   
+            }
+        }
+
         return sb.toString();
     }
+
     public char findKthBit(int n, int k) {
-        if(n==1) return '0';
 
-        String s="0";
-        for(int i=1; i<n; i++){
-            String str=reverse(s);
-            s += "1";
-            s +=str;
+        if (n == 1) return '0';
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("0");
+
+        for (int i = 1; i < n; i++) {
+            String str = reverse(sb);
+            sb.append("1");
+            sb.append(str);
         }
-        char ans=s.charAt(k-1);
-        return ans;
 
-
-
+        return sb.charAt(k - 1);
     }
 }
