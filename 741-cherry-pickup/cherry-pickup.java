6f4bcab558ae3bb18d4393c4cf1d5b67b21fbaf6
Aakash Kushwaha <aakaashkumar898@gmail.com>
2026-04-06 22:53:34 +0530

    Time: 100 ms (14.97%) | Memory: 281.6 MB (13.87%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -36,32 +36,35 @@ class Solution {
         
     // }
     HashMap<String,Integer> hm=new HashMap<>();
-    private int solve(int r1,int c1,int r2,int c2,int[][] grid){
+    private int solve(int r1,int c1,int r2,int c2,int[][] grid,Integer[][][][] dp){
         if(r1<0 || r2<0 || c1 < 0 || c2<0 || grid[r1][c1]==-1 || grid[r2][c2]==-1)  return Integer.MIN_VALUE;
 
         if(r1==0 && c1==0 && r2==0 && c2==0) return grid[r1][c1];
 
         if (r1 + c1 != r2 + c2)
             return Integer.MIN_VALUE;
-        String key=r1+" "+c1+" "+r2+" "+c2;
-        if(hm.containsKey(key)) return hm.get(key);
 
-        int t1=solve(r1-1,c1,r2-1,c2,grid);
-        int t2=solve(r1,c1-1 , r2 ,c2-1,grid);
-        int t3=solve(r1-1,c1,r2,c2-1,grid);
-        int t4=solve(r1,c1-1,r2-1,c2,grid);
+        // String key=r1+" "+c1+" "+r2+" "+c2;
+        // if(hm.containsKey(key)) return hm.get(key);
+
+        if(dp[r1][c1][r2][c2]!=null) return dp[r1][c1][r2][c2];
+
+        int t1=solve(r1-1,c1,r2-1,c2,grid,dp);
+        int t2=solve(r1,c1-1 , r2 ,c2-1,grid,dp);
+        int t3=solve(r1-1,c1,r2,c2-1,grid,dp);
+        int t4=solve(r1,c1-1,r2-1,c2,grid,dp);
 
         int best=Math.max(Math.max(t1,t2),Math.max(t4,t3));
         //bug tha what if minimum aa jaye best hi matlab Integer.MIN_VALUE;
         if(best==Integer.MIN_VALUE) {
-            hm.put(key,best);
-            return best;
+            // hm.put(key,best);
+            return dp[r1][c1][r2][c2]=best;
         }
         int cherry=0;
         if(r1==r2 && c1==c2) cherry=grid[r1][c1];
         else cherry=grid[r1][c1] + grid[r2][c2];
-        hm.put(key,best + cherry);
-        return best + cherry;
+        // hm.put(key,best + cherry);
+        return dp[r1][c1][r2][c2]=best + cherry;
     }
     public int cherryPickup(int[][] grid) {
         int n=grid.length;
@@ -74,7 +77,9 @@ class Solution {
 
         // return forward + backward;
 
-        int ans=solve(n-1,n-1,n-1,n-1,grid);
+        Integer[][][][] dp=new Integer[n+1][n+1][n+1][n+1];
+
+        int ans=solve(n-1,n-1,n-1,n-1,grid,dp);
         if(ans==Integer.MIN_VALUE) return 0;
 
         return ans;
