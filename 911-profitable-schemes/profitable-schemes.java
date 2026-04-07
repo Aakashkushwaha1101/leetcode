9e88694f4d57446c0ffa1025ad7d584dd8893acd
Aakash Kushwaha <aakaashkumar898@gmail.com>
2026-04-07 10:53:36 +0530

    Time: 136 ms (7.55%) | Memory: 243.6 MB (5.14%) - LeetSync

--- 911-profitable-schemes/profitable-schemes.java
+++ 911-profitable-schemes/profitable-schemes.java
@@ -1,17 +1,17 @@
 class Solution {
     int m=1000000007;
     // HashMap<String,Integer> map=new HashMap<>();
-    private int knapsack(int idx,int n,int mp,int[] group,int profit,int[] profits,int[][][] dp){
+    private int knapsack(int idx,int n,int mp,int[] group,int profit,int[] profits,Integer[][][] dp){
         if(idx==profits.length) {
             return profit >= mp ? 1 :0;
         }
-        if(dp[idx][n][profit]!= -1) return dp[idx][n][profit];
+        if(dp[idx][n][profit]!= null) return dp[idx][n][profit];
         // String key=idx+" "+n+" "+profit;
         // if(map.containsKey(key)) return map.get(key);
         int take=0 , skip=0;
         if(n >= group[idx]){
-            int profi=Math.min(mp,profit+profits[idx]);
-            take=knapsack(idx+1,n-group[idx],mp,group,profi,profits,dp);
+            int newProfit=Math.min(mp,profit+profits[idx]);
+            take=knapsack(idx+1,n-group[idx],mp,group,newProfit,profits,dp);
         }
         skip=knapsack(idx+1,n,mp,group,profit,profits,dp);
         int ans=(take + skip)%m;
@@ -21,12 +21,7 @@ class Solution {
     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
 
         int len=group.length;
-        int[][][] dp=new int[101][101][101];
-        for(int[][] d : dp){
-            for(int[] x: d){
-                Arrays.fill(x,-1);
-            }
-        }
+        Integer[][][] dp=new Integer[101][101][101];
         return knapsack(0,n,minProfit,group,0,profit,dp);
     }
 }
