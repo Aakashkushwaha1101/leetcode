a23519bc5c41338183a0d3203f4245836be2eb4e
Aakash Kushwaha <aakaashkumar898@gmail.com>
2026-04-07 16:06:31 +0530

    Time: 8 ms (53.95%) | Memory: 47 MB (99.69%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -1,19 +1,19 @@
 class Solution {
+    
     public long minimumSteps(String s) {
+        int n=s.length();
 
-        char[] ch = s.toCharArray();
-        int ones = 0;   // number of 1s seen so far
-        long count = 0;
+        long one=0;
+        long count=0;
 
-        for (char c : ch) {
-            if (c == '1') {
-                ones++;      // 1 needs to be jumped over by future 0
-            } else {
-                count += ones; // 0 has to swap past all previous 1s
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)=='1'){
+                one++;
+            }else{
+                count +=one;
             }
         }
 
         return count;
     }
 }
-    
