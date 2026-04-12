e696268337899b7683e01186c3f59b4ab50d1d8a
Aakash Kushwaha <aakaashkumar898@gmail.com>
2026-04-12 11:19:02 +0530

    Time: 14 ms (59.05%) | Memory: 48.6 MB (49.86%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,19 +1,52 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] nge = new int[n];
+        Stack<Integer> st=new Stack<>();
 
-        for (int i = 0; i < n; i++) {
-            nge[i] = -1; // nhi mila nge to -1  hi rahega
-            for (int j = 1; j < n; j++) {           
-                int next = nums[(i + j) % n];        // circular indexing handle kar rhe h
-                if (next > nums[i]) {
-                    nge[i] = next;
-                    break;
+        // int  maxElem=Integer.MIN_VALUE;
+        // int idx=-1;
+        // for(int i=0; i<n; i++){
+        //    if( maxElem < nums[i]){
+        //          maxElem=nums[i];
+        //          idx=i;
+        //    }
+        // }
+
+        // st.push(maxElem);
+
+        for(int i=n-1; i>=0; i--){
+            st.push(nums[i]);;
+        }
+        int[] nge=new int[n];
+        // nge[idx]=-1;
+
+        for(int i=n-1; i>=0; i--){    //last se chalenge
+            if(!st.isEmpty()){
+
+                if(st.peek() > nums[i]){
+                    nge[i]=st.peek();
+                    st.push(nums[i]);
                 }
+                else{
+
+                    while((!st.isEmpty()) && nums[i] >= st.peek()){
+                        st.pop();
+                    }
+
+                    if(!st.isEmpty()){
+                        nge[i]=st.peek();
+                        st.push(nums[i]);
+                    }else{
+                        nge[i]=-1;
+                        st.push(nums[i]);
+                    }
+                }
+                
+            }
+            else{
+                st.push(nums[i]);
             }
         }
-
         return nge;
     }
 }
