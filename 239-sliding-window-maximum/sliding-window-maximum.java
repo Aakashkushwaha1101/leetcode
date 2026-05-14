038def99ad5081d27887319000a27b1a7353514d
Aakash Kushwaha <aakaashkumar898@gmail.com>
2026-05-14 12:33:37 +0530

    Time: 30 ms (70.82%) | Memory: 146.7 MB (56.63%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,36 +1,30 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
-        if (n == 0 || k == 0) {
-            return new int[0];
-        }
+        Deque<Integer> dq=new ArrayDeque<>();
 
-        int[] ans = new int[n - k + 1];
+        int[] ans=new int[n-k+1]; //window yhii tkk move hogi to itne hi milega ans
+        int l=0;
+        for(int i=0; i<n; i++){
 
-        Deque<Integer> dq = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-
-            // Remove indices out of current window
-            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+            //yhaa hum window ko maintain kar rahe last index htaa de rahe hai if window invalid ho
+            while(!dq.isEmpty() && dq.peekFirst()<=i-k){
                 dq.pollFirst();
             }
 
-            // Remove smaller elements from back
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+            //still hum monotonic decreasing maintain kar rahe to agar hum koi greater element paate hai current me to hme isase cchote vaale hatane honge , so that we can maintain decreasing order
+            while(!dq.isEmpty() && nums[i] > nums[dq.peekLast()]){
                 dq.pollLast();
             }
 
-            // Add current index
+            //simply push element at last matlab rear end se element ko dalenge deque me
             dq.offerLast(i);
 
-            // Store answer
-            if (i >= k - 1) {
-                ans[i - k + 1] = nums[dq.peekFirst()];
+            //dq hum maitain kar rahe decreasing order me to hmesha max element first pe milega
+            if(i >= k-1){
+                ans[l++]=nums[dq.peekFirst()];
             }
         }
-
         return ans;
     }
 }
-
