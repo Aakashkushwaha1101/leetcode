ee6b09a4b3294ff8390fb105aa9284ccb6746385
Aakash Kushwaha <aakaashkumar898@gmail.com>
2026-05-29 22:27:26 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (70.43%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    // Function to calculate sum of digits
+    public int digitSum(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
+    public int minElement(int[] nums) {
+
+        int min = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            int sum = digitSum(num);
+            min = Math.min(min, sum);
+        }
+
+        return min;
+    }
+}
